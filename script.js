9c0bd4bbbf574d6a632993c6249180b38547d9cb
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-06 09:37:28 +0100

    Clear all root.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function setup() {
  }
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";rootElem.innerHTML = "";
 }
 
 window.onload = setup;
