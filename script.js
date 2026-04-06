9a86ba09e5287abd3eb7cfd83a25c912f59c39a7
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-06 09:14:46 +0100

    Initiate this function to format the season and episodes.

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
+ function formatEpisodeCode(season, episode){
+  const seasonStr = string(season).padStart(2,"0")
+  const episodeStr = string(episode).padStart(2,"0")
+  return `S${seasonStr}E${episodeStr}`;
+ }
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
