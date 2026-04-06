7886df0ce0e505a9c59d0ef6b8415f4bce87eb95
tasleemadedokun <adedokuntasleem@gmail.com>
2026-04-06 09:59:34 +0100

    Implement new functions to get summary, add images and also title.

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
  function formatEpisodeCode(season, episode){
-  const seasonStr = string(season).padStart(2,"0")
-  const episodeStr = string(episode).padStart(2,"0")
+  const seasonStr = String(season).padStart(2,"0")
+  const episodeStr = String(episode).padStart(2,"0")
   return `S${seasonStr}E${episodeStr}`;
  }
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.innerHTML = "";rootElem.innerHTML = "";
-}
+  rootElem.innerHTML = "";
 
+episodeList.forEach(episode => {
+  const card = document.createElement("div");
+  card.className = "episode-card";
+
+  const title = document.createElement("h2");
+  title.textContent = `${episode.name} - ${formatEpisodeCode(episode.season, episode.number)}`;
+
+  const img = document.createElement("img");
+  img.src = episode.image.medium;
+  img.alt = episode.name;
+
+   const summary = document.createElement("p");
+  summary.innerHTML = episode.summary;
+
+  card.appendChild(title);
+  card.appendChild(img);
+  card.appendChild(summary);
+
+  rootElem.appendChild(card);
+})
+  const credit = document.createElement("p");
+credit.innerHTML = `Data originally from <a href="https://tvmaze.com/" target="_blank">TVMaze.com</a>`;
+rootElem.appendChild(credit);
+}
 window.onload = setup;
